25f8c3d68c018be370cbf132ac8b72c81121499e
b3 G0R <egor@barvashov.ru>
2023-11-18 22:38:08 +0300

    try work catboost

--- submission/inference.py
+++ submission/inference.py
@@ -1,10 +1,26 @@
-import joblib
 import pandas as pd
+import numpy as np
+import re
+import matplotlib.pyplot as plt
+import seaborn as sns
 
+import sklearn
+import catboost
+from catboost import CatBoostClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import roc_auc_score
+from sklearn.metrics import roc_curve
+from sklearn.metrics import f1_score
+import joblib
 
-model = joblib.load('submission/models/model.pkl')
+from tqdm.notebook import tqdm_notebook
+from warnings import filterwarnings
+filterwarnings('ignore')
 
-new_data = pd.read_csv('data/test_data.csv')
-
-new_data = transactions_test.groupby(transactions_test.index)\
-                            .progress_apply(features_creation_advanced).unstack(-1)
+tr_mcc_codes = pd.read_csv('data/mcc_codes.csv', sep=';', index_col='mcc_code')
+tr_types = pd.read_csv('data/trans_types.csv', sep=';', index_col='trans_type')
+transactions = pd.read_csv('data/transactions.csv', index_col='client_id')
+gender_train = pd.read_csv('data/train.csv', index_col='client_id')
+gender_test = pd.read_csv('data/test.csv', index_col='client_id')
+transactions_train = transactions.join(gender_train, how='inner')
+transactions_test = transactions.join(gender_test, how='inner')
